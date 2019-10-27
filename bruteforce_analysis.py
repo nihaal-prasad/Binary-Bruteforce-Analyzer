e08e944d39c10d97863c908bc03202b6464c864a
Nihaal <TigerThePredator>
2019-10-27 14:20:28 -0400

    Changed description for start, stop, and -e arguments.

--- bruteforce_analysis.py
+++ bruteforce_analysis.py
@@ -10,8 +10,8 @@ import concurrent.futures
 # Setup the argument parser
 parser = argparse.ArgumentParser(description="Analyzes specified lines of code by executing the code using given input values, recording the output, and graphing the result.", usage='%(prog)s [options] filename start stop input output range')
 parser.add_argument("filename", help="The name of the executable you would like to bruteforce.")
-parser.add_argument("start", help="The first breakpoint will be set at this location, where the input register or memory location will be changed to the next value in the range.")
-parser.add_argument("stop", help="The second breakpoint will be set at this location, where the output will be recorded.")
+parser.add_argument("start", help="The first breakpoint will be set at this location. At the breakpoint, the input register or memory location will be changed to the next value in the range.")
+parser.add_argument("stop", help="The second breakpoint will be set at this location. At the breakpoint, the output will be recorded.")
 parser.add_argument("input", help="The register or memory location that contains the input value that should be bruteforced. Example: \"eax\". If using a memory location, please specify the location using m[location]. Example: \"m[rbp-0x8]\".")
 parser.add_argument("output", help="The register or memory location that contains the output values that should be checked after the code is executed. Example: \"eax\". If using a memory location, please specify the location using m[location]. Example: \"m[rbp-0x8]\".")
 parser.add_argument("range", help="The range of values that should be used for the input during the bruteforce process. Should be in the form \"[lower,upper]\" or \"[lower,upper,step]\". For example: [0,101,5] will use 0, 5, 10, ..., 95, 100 as the input values to be bruteforced. These must be in base 10 (hexadecimal or binary will not work).")
@@ -19,7 +19,7 @@ parser.add_argument("-t", "--threads", nargs='?', dest="threads", default="5", h
 parser.add_argument("-in", "--standard-input", nargs='?', dest='input_file', default='', help="Uses the \'dor stdin=[INPUT_FILE]\' command in radare2 to make the executable read standard input from a given file instead of having the user type it in.")
 parser.add_argument("-il", "--input-length", nargs='?', dest='input_length', default='1', help="The amount of bytes placed at the input memory location. Default value is 1, but this will be automatically adjusted if it is too small. Is only used if the input is a memory location and not a register.")
 parser.add_argument("-ol", "--output-length", nargs='?', dest='output_length', default='1', help="The amount of bytes read at the output memory location. Must be equal to either 1, 2, 4, or 8. Default value is 1. Is only used if the output is a memory location and not a register.")
-parser.add_argument("-e", "--execute", nargs='?', dest='commands', type=str, default='', help="Executes the given r2 commands in radare2 right after the debugger hits the first breakpoint, but before the input value is set.")
+parser.add_argument("-e", "--execute", nargs='?', dest='commands', type=str, default='', help="Executes the given r2 commands in radare2 right after the debugger hits the first breakpoint, but before the input value is set. Example: -e \"dr ebx = 7\" will always set ebx equal to 7 at the first breakpoint. Multiple commands can be separated by a semicolon.")
 
 # Parse all of the arguments
 args = parser.parse_args()
